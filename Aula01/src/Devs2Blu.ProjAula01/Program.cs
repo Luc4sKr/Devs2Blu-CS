634fcc4628ebfd6033258f96ef5fad349e97dd1a
Lucas <lucaskreuch77@gmail.com>
2022-09-12 22:55:12 -0300

    update

---
+++ Aula01/src/Devs2Blu.ProjAula01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjAula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaração de variáveis
+            string nomeUsuario, cidadeUsuario;
+            string formatacaoDados = "Inicialização de variável...\n";
+
+            // Entrada de dados
+            // - Nome
+            Console.Write("Informe seu nome: ");
+            nomeUsuario = Console.ReadLine();
+
+            // - Cidade
+            Console.Write("Informe sua cidade: ");
+            cidadeUsuario = Console.ReadLine();
+
+            // Processamento
+            // - Boas vindas (Nome)
+            formatacaoDados += $"Seja bem-vindo, {nomeUsuario}!\n\n";
+
+            // - Cidade do Usuário
+            formatacaoDados += $"Localidade: {cidadeUsuario}";
+
+            // Apresentação dos dados
+            Console.WriteLine(formatacaoDados);
+
+            // Pausa antes de encerrar
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aula02/src/Devs2Blu.ProjAula02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjAula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeCandidato, situacaoCandidato, idadeInput, notaInput;
+            int idadeCandidato = 0;
+            float notaCandidato = 0;
+            bool idadeValida = false, notaValida = false;
+
+            Console.WriteLine("--- Bem vindo ao sistema ScoolSystem ---\n");
+
+            Console.Write("Informe o nome do Aluno: ");
+            nomeCandidato = Console.ReadLine();
+
+            while (!idadeValida)
+            {
+                Console.Write("Informe a idade do Aluno: ");
+                idadeInput = Console.ReadLine();
+                idadeValida = Regex.IsMatch(idadeInput, @"^[0-9]+$");
+                if (!idadeValida)
+                {
+                    Console.WriteLine("Idade inválida!");
+                }
+                else
+                {
+                    idadeCandidato = Convert.ToInt32(idadeInput);
+                }
+            }
+
+            while (!notaValida)
+            {
+                Console.Write("Informe a nota do Aluno: ");
+                notaInput = Console.ReadLine();
+                notaValida = Regex.IsMatch(notaInput, @"^[0-9]+$");
+                if (!notaValida)
+                {
+                    Console.WriteLine("Nota inválida");
+                }
+                else
+                {
+                    notaCandidato = Convert.ToInt32(notaInput);
+                }
+            }
+
+            if (notaCandidato == 10)
+            {
+                situacaoCandidato = "Aprovado com exelência";
+            }
+            else
+            {
+                if (notaCandidato >= 7)
+                {
+                    situacaoCandidato = "Aprovado";
+                }
+                else
+                {
+                    if (notaCandidato >= 5)
+                    {
+                        situacaoCandidato = "Em recuperação";
+                    }
+                    else
+                    {
+                        situacaoCandidato = "Reprovado";
+                    }
+                }
+            }
+
+            Console.WriteLine($"\nNome: {nomeCandidato} - Idade: {idadeCandidato}");
+            Console.WriteLine($"Nota: {notaCandidato} - Situação: {situacaoCandidato}\n");
+
+            Console.Write("ENTER para encerrar...");
+            Console.ReadLine();
+        }
+    }
+}
