49b1f024a9a1ee24ea885ab129aebdf132669d71
Lucas <lucaskreuch77@gmail.com>
2022-09-20 22:15:43 -0300

    [OPP] - Mocks

--- Aula11-OOP - parte 3/Revisao/SlnRevisaoOOP/src/ConsoleApp1.ProjetosAula.OOP03.Main/Program.cs
+++ Aula11-OOP - parte 3/Revisao/SlnRevisaoOOP/src/ConsoleApp1.ProjetosAula.OOP03.Main/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ConsoleApp1.ProjetosAula.OOP03.Main.Utils;
+using Devs2Blu.ProjetosAula.OOP03.Models.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,27 @@ namespace ConsoleApp1.ProjetosAula.OOP03.Main
 {
     internal class Program
     {
+        public static Mocks Mock { get; set; }
+
         static void Main(string[] args)
         {
+            Mock = new Mocks();
+            ViewListPacientes();
+        }
+
+        public static void ViewListPacientes()
+        {
+            Console.Clear();
+
+            foreach (Paciente paciente in Mock.ListaPacientes)
+            {
+                Console.WriteLine("\n---------------------------------------------");
+                Console.WriteLine($"Paciente: {paciente.CodigoPaciente}");
+                Console.WriteLine($"Nome: {paciente.Nome}");
+                Console.WriteLine($"CPF: {paciente.CGCCPF}");
+                Console.WriteLine($"Convenio: {paciente.Convenio}");
+                Console.WriteLine("---------------------------------------------\n");
+            }
         }
     }
 }
