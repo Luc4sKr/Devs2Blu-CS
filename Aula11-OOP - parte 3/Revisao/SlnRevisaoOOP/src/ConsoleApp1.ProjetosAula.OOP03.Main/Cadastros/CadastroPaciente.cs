fec09fff72fe241807964a97dae7932628f2134e
Lucas <lucaskreuch77@gmail.com>
2022-09-21 21:11:32 -0300

    [SISTEMA DE GERENCIAMENTO DE CLÍNICA] Continuação da Aula 11

---
+++ Aula11-OOP - parte 3/Revisao/SlnRevisaoOOP/src/ConsoleApp1.ProjetosAula.OOP03.Main/Cadastros/CadastroPaciente.cs
@@ -0,0 +1,79 @@
+﻿using ConsoleApp1.ProjetosAula.OOP03.Main.Utils;
+using ConsoleApp1.ProjetosAula.OOP03.Main.Utils.Enums;
+using Devs2Blu.ProjetosAula.OOP03.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.ProjetosAula.OOP03.Main.Cadastros
+{
+    public class CadastroPaciente
+    {
+        public CadastroPaciente()
+        {
+
+        }
+
+        public void MunuCadastro()
+        {
+            Int32 opcao = 0;
+
+            do
+            {
+                Console.WriteLine("------------- CADASTRO PACIENTES -------------");
+                Console.WriteLine("1 - Lista pacientes");
+                Console.WriteLine("2 - Cadastro de pacientes");
+                Console.WriteLine("3 - Alterar pacientes");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("----------------------------------------------");
+                Console.Write("-> ");
+                Int32.TryParse(Console.ReadLine(), out opcao);
+
+                switch (opcao)
+                {
+                    case (int)MenuEnums.LISTAR:
+                        ListarPacientes();
+                        break;
+                    case (int)MenuEnums.CADASTRAR:
+                        CadastarPaciente();
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (opcao != (int)MenuEnums.SAIR);
+        }
+
+        public void ListarPacientes()
+        {
+            Console.Clear();
+
+            foreach (Paciente paciente in Program.Mock.ListaPacientes)
+            {
+                Console.WriteLine("\n----------------------------------------------");
+                Console.WriteLine($"Paciente: {paciente.CodigoPaciente}");
+                Console.WriteLine($"Nome: {paciente.Nome}");
+                Console.WriteLine($"CPF: {paciente.CGCCPF}");
+                Console.WriteLine($"Convenio: {paciente.Convenio}");
+                Console.WriteLine("----------------------------------------------\n");
+            }
+        }
+
+        public void CadastarPaciente()
+        {
+
+        }
+
+        public void AlterarPaciente()
+        {
+
+        }
+        
+        public void ExcluirPaciete()
+        {
+
+        }
+    }
+}
--- Aula11-OOP - parte 3/Revisao/SlnRevisaoOOP/src/ConsoleApp1.ProjetosAula.OOP03.Main/Program.cs
+++ Aula11-OOP - parte 3/Revisao/SlnRevisaoOOP/src/ConsoleApp1.ProjetosAula.OOP03.Main/Program.cs
@@ -1,4 +1,6 @@
-﻿using ConsoleApp1.ProjetosAula.OOP03.Main.Utils;
+﻿using ConsoleApp1.ProjetosAula.OOP03.Main.Cadastros;
+using ConsoleApp1.ProjetosAula.OOP03.Main.Utils;
+using ConsoleApp1.ProjetosAula.OOP03.Main.Utils.Enums;
 using Devs2Blu.ProjetosAula.OOP03.Models.Model;
 using System;
 using System.Collections.Generic;
@@ -12,10 +14,39 @@ namespace ConsoleApp1.ProjetosAula.OOP03.Main
     {
         public static Mocks Mock { get; set; }
 
+
         static void Main(string[] args)
         {
+            int opcao;
             Mock = new Mocks();
-            ViewListPacientes();
+
+            do
+            {
+                Console.WriteLine("---- SISTEMA DE GERENCIAMENTO DE CLÍNICAS ----");
+                Console.WriteLine("10 - Cadastro de Pacientes");
+                Console.WriteLine("20 - Cadastro de Médicos");
+                Console.WriteLine("30 - Cadastro de Recepcionistas");
+                Console.WriteLine("40 - Cadastro de Fornecedores");
+                Console.WriteLine("50 - Agenda");
+                Console.WriteLine("60 - Prontuario");
+                Console.WriteLine("70 - Financeiro");
+                Console.WriteLine("00 - Sair");
+                Console.WriteLine("----------------------------------------------");
+                Console.Write("-> ");
+                Int32.TryParse(Console.ReadLine(), out opcao);
+
+                switch (opcao)
+                {
+                    case (int)MenuEnums.CAD_PACIENTE:
+                        CadastroPaciente ModuloCadastroPacientes = new CadastroPaciente();
+                        ModuloCadastroPacientes.MunuCadastro();
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (opcao != (int)MenuEnums.SAIR);
+            
         }
 
         public static void ViewListPacientes()
