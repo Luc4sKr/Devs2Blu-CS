8e6d7defee93b1c47acd956329c4efdaf17eda80
Lucas <lucaskreuch77@gmail.com>
2022-09-30 22:12:51 -0300

    [BD] - Primaira aula sobre banco de dados

---
+++ Banco de Dados/Aula 01/SQL_DELETE.sql
@@ -0,0 +1,23 @@
+-- INSERT de teste
+INSERT INTO pessoa (nome,
+                    datanascimento,
+                    idade,
+                    status)
+VALUES ('Josefino', '2005-03-22', 17, 1);
+
+/*DELETE*/
+
+DELETE FROM pessoa
+WHERE id IN (5, 6);
+
+-- Apaga o último registro ---------------------------------------------
+DELETE FROM pessoa
+WHERE max(id); -- Continua na próxima aula
+-- ----------------------------------------------------------------------
+
+
+
+
+-- WHERE nome LIKE 'Josefino';
+-- J% -> Deleta tudo que começa com 'J'
+-- %sefi% -> Deleta tudo que tem 'sefi' na string
