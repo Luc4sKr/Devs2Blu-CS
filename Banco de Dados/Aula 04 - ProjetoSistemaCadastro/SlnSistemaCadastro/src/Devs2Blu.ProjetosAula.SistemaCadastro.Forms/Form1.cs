be90597c07ce50c6d2fdc7783caad23dae2a6735
Lucas <lucaskreuch77@gmail.com>
2022-10-07 20:25:18 -0300

    [Sistema Cadastro] - Tela Cadastro

---
+++ Banco de Dados/Aula 04 - ProjetoSistemaCadastro/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Devs2Blu.ProjetosAula.SistemaCadastro.Forms.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms
+{
+    public partial class Form1 : Form
+    {
+        public MySqlConnection Conn { get; set; }
+        public ConvenioRepository ConvenioRepository = new ConvenioRepository();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region TesteConexao
+            /*Conn = ConnectionMySQL.GetConnection();
+
+            if (Conn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Conexão efetuada com sucesso!", "Conexão ao MySQL", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Conn.Close();
+            }*/
+            #endregion
+            PopulaComboConvenio();
+        }
+
+        #region Methods
+
+        private void PopulaComboConvenio()
+        {
+            var listConvenios = ConvenioRepository.FetchAll();
+            cboConvenio.DataSource = new BindingSource(listConvenios, null);
+            cboConvenio.DisplayMember = "nome";
+            cboConvenio.ValueMember = "id";
+        }
+        #endregion
+
+        private void rdFisica_CheckedChanged(object sender, EventArgs e)
+        {
+            lblCGCCPF.Text = "CPF";
+            lblCGCCPF.Visible = true;
+        }
+
+        private void rdJuridica_CheckedChanged(object sender, EventArgs e)
+        {
+            lblCGCCPF.Text = "CNPJ";
+            lblCGCCPF.Visible = true;
+        }
+    }
+}
