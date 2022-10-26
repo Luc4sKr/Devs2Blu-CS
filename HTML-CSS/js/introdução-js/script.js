14b034acb6db99c4c9bb4433409285013fd7d96e
Lucas <lucaskreuch77@gmail.com>
2022-10-25 23:24:09 -0300

    Introdução ao JavaScript

---
+++ HTML-CSS/js/introdução-js/script.js
@@ -0,0 +1,14 @@
+console.log("Script loaded");
+
+var nome, idade;
+
+// nome = prompt("Informe seu nome")
+// console.log(`Nome: ${nome}`)
+
+// idade = prompt("Infome sua idade")
+// console.log(`Idade: ${idade}`)
+
+// alert("Hello world")
+
+var bloco = document.getElementById("bloco");
+bloco.style.margin = "5em"
