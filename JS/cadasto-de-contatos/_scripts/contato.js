383d0f7e9d10df901f1bc9a2048544f3765c807d
Lucas <lucaskreuch77@gmail.com>
2022-10-30 16:26:23 -0300

    [Em desenvolvimento] - Cadastro de contatos feito

---
+++ JS/cadasto-de-contatos/_scripts/contato.js
@@ -0,0 +1,12 @@
+class Contato {
+    constructor(nome, email) {
+        this.nome = nome;
+        this.email = email;
+    }
+}
+
+class ListaContatos {
+    constructor() {
+        this.contatos = [];
+    }
+}
--- JS/cadasto-de-contatos/_scripts/dashboard.js
+++ JS/cadasto-de-contatos/_scripts/dashboard.js
@@ -1,23 +1,48 @@
-const listaContatos = [];
-listaContatos.push({nome: "Lucas", email:"lucas@gmail.com"})
-
-const listarContatos = () => {
-    listaContatos.forEach((c) => {
-        var linha = document.createElement('tr');
-        var colNome = document.createElement('td');
-        var colEmail = document.createElement('td');
-
-        $(colNome).html(c.nome);
-        $(colEmail).html(c.email);
-        $(linha).append(colNome).append(colEmail);
-        $('#lista-contatos').append(linha);
-    });
-}
+var listaContatos = new ListaContatos()
 
 $(document).ready(() => {
     if (!verificaLogin()) {
         window.location.href = "../index.html";
     }
 
-    listarContatos();
+    if (getListaContatos()) {
+        listarContatos();
+    }
+
+    $("#btn-cadastrar").click((e) => {
+        addContato();
+        listarContatos();
+    });
 });
+
+// Functions
+const listarContatos = () => {
+    $("#lista-contatos").children().remove();
+    JSON.parse(getListaContatos()).contatos.forEach((contato) => {
+        var linha = document.createElement('tr');
+        var colNome = document.createElement('td');
+        var colEmail = document.createElement('td');
+
+        $(colNome).html(contato.nome);
+        $(colEmail).html(contato.email);
+        $(linha).append(colNome).append(colEmail);
+        $("#lista-contatos").append(linha);
+    });
+}
+
+function addContato() {
+    var contato = new Contato(
+        $("#input-nome").val(), $("#input-email").val()
+    );
+
+    if (getListaContatos()) {
+        listaContatos.contatos = JSON.parse(getListaContatos()).contatos;
+    }
+
+    listaContatos.contatos.push(contato);
+    localStorage.setItem("listaContatos", JSON.stringify(listaContatos));
+}
+
+function getListaContatos() {
+    return localStorage.getItem("listaContatos");
+}
