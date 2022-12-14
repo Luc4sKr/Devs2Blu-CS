d9dc5c0e80bc534ef3f0ea31e2049fbed9139af4
Lucas <lucaskreuch77@gmail.com>
2022-12-13 22:49:38 -0300

    Projeto MVC com SQL Server

---
+++ MVC/SlnMVCSQLServerApp2/src/Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web/Migrations/20221214004156_MigTabelasSistema.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigTabelasSistema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categorias",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categorias", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    preco = table.Column<double>(type: "float", nullable: false),
+                    quantidade = table.Column<int>(type: "int", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produtos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_produtos_categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "categorias",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "categorias",
+                columns: new[] { "id", "nome" },
+                values: new object[,]
+                {
+                    { 1, "Alimentos não perecíveis" },
+                    { 2, "Laicínios" },
+                    { 3, "Limpeza" },
+                    { 4, "Bebidas não alcoólicas" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "produtos",
+                columns: new[] { "id", "CategoriaId", "nome", "preco", "quantidade" },
+                values: new object[,]
+                {
+                    { 1, 1, "Arroz", 10.0, 5 },
+                    { 2, 1, "Feijão", 15.0, 15 },
+                    { 3, 2, "Queijo", 20.0, 20 },
+                    { 4, 2, "Iogurte", 10.0, 5 },
+                    { 5, 4, "Suco de Laranja 1L", 5.0, 5 },
+                    { 6, 4, "Coca-Cola 2L", 8.0, 30 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_produtos_CategoriaId",
+                table: "produtos",
+                column: "CategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "produtos");
+
+            migrationBuilder.DropTable(
+                name: "categorias");
+        }
+    }
+}
---
+++ MVC/SlnMVCSQLServerApp2/src/Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web/Models/ContextoDatabase.cs
@@ -0,0 +1,49 @@
+﻿using Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Devs2Blu.ProjetosAula.MVCSQLServerApp2.Web.Models
+{
+    public class ContextoDatabase : DbContext
+    {
+        public ContextoDatabase(DbContextOptions<ContextoDatabase> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Mapeamento de Relacionamento
+            modelBuilder.Entity<Produto>()
+                .HasOne(p => p.Categoria)
+                .WithMany(c => c.Produtos)
+                .HasForeignKey(p => p.CategoriaId);
+
+            // Seed
+            modelBuilder.Entity<Categoria>()
+                .HasData(
+                    new { Id = 1, Nome = "Alimentos não perecíveis" },
+                    new { Id = 2, Nome = "Laicínios" },
+                    new { Id = 3, Nome = "Limpeza" },
+                    new { Id = 4, Nome = "Bebidas não alcoólicas" }
+                );
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                    new { Id = 1, Nome = "Arroz", Preco = 10.00, Quantidade = 5, CategoriaId = 1},
+                    new { Id = 2, Nome = "Feijão", Preco = 15.00, Quantidade = 15, CategoriaId = 1 },
+                    new { Id = 3, Nome = "Queijo", Preco = 20.00, Quantidade = 20, CategoriaId = 2 },
+                    new { Id = 4, Nome = "Iogurte", Preco = 10.00, Quantidade = 5, CategoriaId = 2 },
+                    new { Id = 5, Nome = "Suco de Laranja 1L", Preco = 5.00, Quantidade = 5, CategoriaId = 4 },
+                    new { Id = 6, Nome = "Coca-Cola 2L", Preco = 8.00, Quantidade = 30, CategoriaId = 4 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        #region DbSets
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
+        #endregion
+    }
+}
