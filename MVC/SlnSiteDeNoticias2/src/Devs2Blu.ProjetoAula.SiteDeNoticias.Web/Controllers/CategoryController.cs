a42404c746ffdf8aff367a2275bbddd8706fd8a7
Lucas <lucaskreuch77@gmail.com>
2023-01-17 22:04:06 -0300

    Alterações da aula

---
+++ MVC/SlnSiteDeNoticias2/src/Devs2Blu.ProjetoAula.SiteDeNoticias.Web/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Devs2Blu.ProjetoAula.SiteDeNoticias.Domain.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devs2Blu.ProjetoAula.SiteDeNoticias.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger;
+        private readonly ICategoryService _service;
+
+        public CategoryController(ILogger<CategoryController> logger,
+                                  ICategoryService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            // To list all categories
+            // Get of CategoryRepository through Dependency Injection (ICategoryService)
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
